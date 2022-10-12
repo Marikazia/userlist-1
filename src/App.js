af4be6f84d086ed1d7e9b7f44380c76270156569
Mariana Machohan <debrisofme9@gmail.com>
2022-10-12 21:13:35 +0200

    cc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 
+import React from 'react';
 import './App.css';
 
+
+
+// Тут список пользователей: https://reqres.in/api/users
+
 function App() {
+	const [users, setUsers] = React.useState([]);
+
+	React.useEffect(() => {
+		fetch('https://reqres.in/api/users')
+			.then((res) => res.json())
+			.then((json) => {
+				setUsers(json.data);
+		}).catch();
+	}, []);
+
   return (
 		<>
 			<div className='app'>
