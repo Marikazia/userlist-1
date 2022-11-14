94c6d561a4864843fb4d4c94f7a6df707cfcac03
Mariana Machohan <debrisofme9@gmail.com>
2022-11-14 01:47:28 +0100

    1

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -5,9 +5,9 @@ export const Success = ({ count }) => {
 	return (
 		<div class="success-block">
 			<img src="/assets/success.svg" alt="Success" />
-			<h3>Успешно!</h3>
-			<p>Всем {count} пользователям отправлено приглашение.</p>
-			<button className="send-invite-btn">Назад</button>
+			<h3>Successfully!</h3>
+			<p>To all {count} users has been send an invitation.</p>
+			<button className="send-invite-btn">Back</button>
 		</div>
 	);
 };
