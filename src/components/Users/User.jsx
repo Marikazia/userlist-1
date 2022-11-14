67155301069a6ceec2066c6171dcd6a1687d6e7c
Mariana Machohan <debrisofme9@gmail.com>
2022-11-14 01:47:45 +0100

    success

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -14,6 +14,6 @@ export const User = ({ id, email, first_name, last_name, avatar, onClickInvite, 
 				</p>
 			</div>
 		</div>
-		<img onClick={onClickInvite} className="action-icon" src={`./assets/${isInvited ? 'minus' : 'plus'}.svg`} alt="Action" />
+		<img onClick={() => onClickInvite(id)} className="action-icon" src={`./assets/${isInvited ? 'minus' : 'plus'}.svg`} alt="Action" />
 	</li>
 );
