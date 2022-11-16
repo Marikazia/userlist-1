4f78b2e1d6f956bad55652e5f52a9928b62b4271
Mariana Machohan <debrisofme9@gmail.com>
2022-11-16 17:16:36 +0100

    if lenght >0

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -40,9 +40,12 @@ export const Users = ({ items, isLoading, searchValue, onChangeSearchValue, invi
 						))}
 				</ul>
 			)}
-			<button onClick={onClickSendInvites} className="send-invite-btn">
-				Send invite
-			</button>
+			{invites.length > 0 && (
+				<button onClick={onClickSendInvites} className="send-invite-btn">
+					Send invite
+				</button>
+			)}
+
 		</>
 	);
 };
