e3de67d11ab85f6f1ae38b338fbf195286d1704d
Mariana Machohan <debrisofme9@gmail.com>
2022-12-13 18:40:24 +0100

    style it

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const Success = ({ count }) => {
 	return (
 		<div class="success-block">
-			<img src="/assets/success.svg" alt="Success" />
+			<img className="success-icon" src="/assets/success.svg" alt="Success" />
 			<h3>Successfully!</h3>
 			<p>To all {count} users has been send an invitation.</p>
 			<button onClick={() => window.location.reload()} className="send-invite-btn">Back</button>
